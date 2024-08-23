62e582f2258e186285f5714aecbaeb3337470815
sarism <saraisabelmayalasso@gmail.com>
2024-08-22 22:21:30 -0500

    subir

---
+++ public/src/components/card/card.js
@@ -0,0 +1,42 @@
+class Card extends HTMLElement {
+	static get observedAttributes() {
+		return ['img', 'alt', 'title', 'description', 'txtbutton'];
+	}
+
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+	}
+
+	connectedCallback() {
+		this.render();
+	}
+
+	attributeChangedCallback(propName, oldValue, newValue) {
+		if (oldValue !== newValue) {
+			this[propName] = newValue;
+			this.render();
+		}
+	}
+
+	
+render() {
+    this.shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="./src/components/card/card.css">
+        <div class="product-card">
+        <div id="info">
+            
+            <h1>${this.title}</h1>
+            <p>${this.description}</p>
+            
+            <button id="boton">${this.txtbutton}</button>
+            </div>
+            <img id="fotito" src=${this.img} alt=${this.alt}>
+        </div>
+`;
+}
+}
+
+
+customElements.define('product-card', Card);
+export default Card;
